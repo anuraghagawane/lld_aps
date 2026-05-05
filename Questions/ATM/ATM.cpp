9a913f7408c437449c09cfebc6a73d28a5aa4cf5
anuraghagawane <anuraghagawane10@gmail.com>
2026-05-05 09:52:53 +0530

    ATM final

--- Questions/ATM/ATM.cpp
+++ Questions/ATM/ATM.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <mutex>
+#include <stdexcept>
 #include <unordered_map>
 
 using namespace std;
@@ -9,13 +11,18 @@ class Account {
 private:
   string accountId;
   int balance;
+  mutable mutex mtx;
 
 public:
   Account(string accountId, int balance)
       : accountId(accountId), balance(balance) {}
-  int getBalance() const { return balance; }
+  int getBalance() const {
+    lock_guard<mutex> lock(mtx);
+    return balance;
+  }
 
   bool debit(int amount) {
+    lock_guard<mutex> lock(mtx);
     if (balance < amount)
       return false;
     balance -= amount;
@@ -23,6 +30,7 @@ public:
   }
 
   void credit(int amount) {
+    lock_guard<mutex> lock(mtx);
     if (amount > 0)
       balance += amount;
   }
@@ -32,26 +40,26 @@ class Card {
 private:
   string cardNumber;
   string pin;
-  Account *account;
+  shared_ptr<Account> account;
 
 public:
-  Card(string cardNumber, string pin, Account *account)
+  Card(string cardNumber, string pin, shared_ptr<Account> account)
       : cardNumber(cardNumber), pin(pin), account(account) {}
 
   string getCardNumber() const { return cardNumber; }
 
   bool validatePin(string pin) const { return this->pin == pin; }
-  Account *getAccount() { return account; }
+  shared_ptr<Account> getAccount() { return account; }
 };
 
 class BankService {
 private:
-  unordered_map<string, Card *> cards;
+  unordered_map<string, shared_ptr<Card>> cards;
 
 public:
-  void addCard(Card *card) { cards[card->getCardNumber()] = card; }
+  void addCard(shared_ptr<Card> card) { cards[card->getCardNumber()] = card; }
 
-  Account *authenticate(string cardNumber, string pin) {
+  shared_ptr<Account> authenticate(const string cardNumber, const string pin) {
     if (cards.count(cardNumber) && cards[cardNumber]->validatePin(pin)) {
       return cards[cardNumber]->getAccount();
     }
@@ -63,6 +71,7 @@ public:
 class CashDispenser {
 private:
   map<int, int> notes;
+  mutable mutex mtx;
 
 public:
   CashDispenser() {
@@ -72,6 +81,7 @@ public:
   }
 
   bool canDispense(int amount) const {
+    lock_guard<mutex> lock(mtx);
     int remaining = amount;
     for (auto it = notes.rbegin(); it != notes.rend(); ++it) {
       int denom = it->first;
@@ -85,6 +95,7 @@ public:
   }
 
   void dispense(int amount) {
+    std::lock_guard<std::mutex> lock(mtx);
     int remaining = amount;
     for (auto it = notes.rbegin(); it != notes.rend(); ++it) {
       int denom = it->first;
@@ -110,12 +121,13 @@ public:
 
 class WithdrawTransaction : public Transaction {
 private:
-  Account *account;
+  shared_ptr<Account> account;
   int amount;
-  CashDispenser *dispenser;
+  shared_ptr<CashDispenser> dispenser;
 
 public:
-  WithdrawTransaction(Account *account, int amount, CashDispenser *dispenser)
+  WithdrawTransaction(shared_ptr<Account> account, int amount,
+                      shared_ptr<CashDispenser> dispenser)
       : account(account), amount(amount), dispenser(dispenser) {}
 
   void execute() override {
@@ -136,11 +148,11 @@ public:
 
 class DepositTransaction : public Transaction {
 private:
-  Account *account;
+  shared_ptr<Account> account;
   int amount;
 
 public:
-  DepositTransaction(Account *account, int amount)
+  DepositTransaction(shared_ptr<Account> account, int amount)
       : account(account), amount(amount) {}
 
   void execute() override {
@@ -153,64 +165,151 @@ public:
     cout << "Deposit successfull" << endl;
   }
 };
-class ATM {
-private:
-  BankService *bankService;
-  CashDispenser dispenser;
-  Account *currentAccount;
 
+class ATM;
+
+class ATMState {
 public:
-  ATM(BankService *bankService) : bankService(bankService) {}
-
-  bool authenticate(string cardNumber, string pin) {
-    currentAccount = bankService->authenticate(cardNumber, pin);
-    if (currentAccount) {
-      cout << "Authentication successfull\n";
-      return true;
-    }
-    cout << "Authentication failed\n";
-    return false;
+  virtual void insertCard(ATM &atm, const string &cardNumber) {
+    throw std::runtime_error("Card already inserted");
   }
 
-  void withdraw(int amount) {
-    if (!currentAccount) {
-      cout << "No active session" << endl;
-      return;
-    }
-    unique_ptr<Transaction> txn =
-        make_unique<WithdrawTransaction>(currentAccount, amount, &dispenser);
-    txn->execute();
+  virtual void enterPin(ATM &atm, const string &pin) {
+    throw runtime_error("PIN entry not allowed now");
   }
 
-  void deposit(int amount) {
-    if (!currentAccount) {
-      cout << "No active session" << endl;
-      return;
-    }
-
-    unique_ptr<Transaction> txn =
-        make_unique<DepositTransaction>(currentAccount, amount);
-    txn->execute();
+  virtual void withdraw(ATM &atm, int amount) {
+    throw runtime_error("Not authenticated");
   }
 
-  void endSession() { currentAccount = nullptr; }
+  virtual void deposit(ATM &atm, int amount) {
+    throw std::runtime_error("Not authenticated");
+  }
+
+  virtual void ejectCard(ATM &atm) {
+    throw std::runtime_error("No card to eject");
+  }
+
+  virtual ~ATMState() = default;
 };
 
-int main() {
-  cout << "ATM" << endl;
-  Account acc1("A1", 10000);
-  Card card1("1234", "1111", &acc1);
+class IdleState : public ATMState {
+public:
+  void insertCard(ATM &atm, const string &cardNumber) override;
+};
 
-  BankService bank;
-  bank.addCard(&card1);
+class CardInsertedState : public ATMState {
+public:
+  void enterPin(ATM &atm, const string &amount) override;
+  void ejectCard(ATM &atm) override;
+};
 
-  ATM atm(&bank);
+class AuthenticatedState : public ATMState {
+public:
+  void withdraw(ATM &atm, int amount) override;
+  void deposit(ATM &atm, int amount) override;
+  void ejectCard(ATM &atm) override;
+};
 
-  if (atm.authenticate("1234", "1111")) {
-    atm.withdraw(3700);
-    atm.deposit(2000);
+class ATM {
+private:
+  friend class IdleState;
+  friend class CardInsertedState;
+  friend class AuthenticatedState;
+  unique_ptr<ATMState> state;
+  shared_ptr<BankService> bankService;
+  shared_ptr<CashDispenser> dispenser;
+  shared_ptr<Account> currentAccount;
+  string currentCardNumber;
 
-    atm.endSession();
+  void changeState(unique_ptr<ATMState> newState) { state = move(newState); }
+
+public:
+  ATM(shared_ptr<BankService> bankService)
+      : state(make_unique<IdleState>()), bankService(move(bankService)),
+        dispenser(make_shared<CashDispenser>()) {}
+
+  void insertCard(const string &cardNumber) {
+    state->insertCard(*this, cardNumber);
   }
+
+  void enterPin(const string &pin) { state->enterPin(*this, pin); }
+
+  void withdraw(int amount) { state->withdraw(*this, amount); }
+
+  void deposit(int amount) { state->deposit(*this, amount); }
+
+  void ejectCard() { state->ejectCard(*this); }
+};
+
+void IdleState::insertCard(ATM &atm, const string &cardNumber) {
+  atm.currentCardNumber = cardNumber;
+  atm.changeState(make_unique<CardInsertedState>());
+  cout << "Card inserted: " << cardNumber << "\n";
+}
+
+void CardInsertedState::enterPin(ATM &atm, const string &pin) {
+  auto account = atm.bankService->authenticate(atm.currentCardNumber, pin);
+  if (account) {
+    atm.currentAccount = account;
+    atm.changeState(make_unique<AuthenticatedState>());
+    cout << "Authentication successfull" << endl;
+  } else {
+    cout << "Authentication failed" << endl;
+    atm.currentCardNumber.clear();
+    atm.changeState(make_unique<IdleState>());
+  }
+}
+
+void CardInsertedState::ejectCard(ATM &atm) {
+  std::cout << "Ejecting card: " << atm.currentCardNumber << "\n";
+  atm.currentCardNumber.clear();
+  atm.changeState(std::make_unique<IdleState>());
+}
+
+void AuthenticatedState::withdraw(ATM &atm, int amount) {
+  if (!atm.currentAccount)
+    return;
+  auto txn = make_unique<WithdrawTransaction>(atm.currentAccount, amount,
+                                              atm.dispenser);
+
+  txn->execute();
+}
+
+void AuthenticatedState::deposit(ATM &atm, int amount) {
+  if (!atm.currentAccount)
+    return;
+  auto txn = std::make_unique<DepositTransaction>(atm.currentAccount, amount);
+  txn->execute();
+}
+
+void AuthenticatedState::ejectCard(ATM &atm) {
+  std::cout << "Ejecting card: " << atm.currentCardNumber << "\n";
+  atm.currentAccount.reset();
+  atm.currentCardNumber.clear();
+  atm.changeState(std::make_unique<IdleState>());
+}
+
+int main() {
+  auto acc1 = make_shared<Account>("A1", 10000);
+  auto card1 = make_shared<Card>("1234-5678", "1111", acc1);
+  auto bank = make_shared<BankService>();
+
+  bank->addCard(card1);
+
+  ATM atm(bank);
+
+  atm.insertCard("1234-5678");
+  atm.enterPin("1111");
+  atm.withdraw(2500);
+  atm.deposit(800);
+  atm.ejectCard();
+
+  try {
+    atm.withdraw(100);
+  } catch (const std::runtime_error &e) {
+    std::cout << "Error: " << e.what() << "\n";
+  }
+
   return 0;
 }
